fc95f7f9cca8ed631ae7d278fe421d3294d06b89
algoaddict1 <algoaddictstories@gmail.com>
2025-07-16 12:54:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 from flask import Flask
 from flask_cors import CORS
 from routes import routes
-from dotenv import load_dotenv
-
-load_dotenv()
 
 app = Flask(__name__)
-CORS(app)
+
+CORS(app, resources={r"/*": {"origins": "https://algo-stories-3cyu.vercel.app"}})
 
 app.register_blueprint(routes)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True)
