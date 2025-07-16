4971a6cda3adaec782201b825b55685bba363878
algoaddict1 <algoaddictstories@gmail.com>
2025-07-16 15:55:56 +0200

    Update models.py

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ load_dotenv()
 MONGO_URI = os.getenv("MONGO_URI")
 client = MongoClient(MONGO_URI)
 
-db = client["AlgoAddict"]  # ⚠️ Il nome del tuo database (corretto se creato così)
+db = client["aas_db"] 
 stories_collection = db["stories"]
 likes_collection = db["likes"]
 comments_collection = db["comments"]
